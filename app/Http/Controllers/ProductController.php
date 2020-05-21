879ff7e1c7ca41e411cf5e48ae332988bde14155
DavidPinilla92 <d.pinilla@geekshubs.com>
2020-05-21 12:04:18 +0200

    added ProductController and also auth laravel

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    
+    public function getAll()
+    {
+        return ['Productos'=>[]];
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,23 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::get('/',fn () => 'hola api');
+
+Route::get('/', fn () => 'hola api');
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+Route::group([
+    'prefix' => 'v1'
+], function () {
+
+
+    Route::group([
+        'prefix' => 'products'
+    ], function () {
+
+        Route::get('/','ProductController@getAll');
+
+    });
+
+
+});
